2614fcc5b51b347aba4f1e286f9dabcef858fe46
Sergey Fomenko <fomenko.personal@gmail.com>
2021-05-06 15:54:49 +0000

    feat: add initial cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,66 @@
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__button');
-let editButton = document.querySelector('.profile__button_type_edit');
-let name = document.querySelector('.profile__name');
-let description = document.querySelector('.profile__description');
-let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('#name');
-let descriptionInput = formElement.querySelector('#description');
+// edit user info
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__button');
+const editButton = document.querySelector('.profile__button_type_edit');
+const name = document.querySelector('.profile__name');
+const description = document.querySelector('.profile__description');
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('#name');
+const descriptionInput = formElement.querySelector('#description');
 
-let formSubmitHandler = (evt) => {
+const formSubmitHandler = (evt) => {
   evt.preventDefault();
   name.textContent = nameInput.value;
   description.textContent = descriptionInput.value;
   popup.classList.remove('popup_opened')
 }
 
-let formOpenHandler = () => {
+const formOpenHandler = () => {
   nameInput.value = name.textContent;
   descriptionInput.value = description.textContent;
   popup.classList.add('popup_opened')
 }
 
-let formCloseHandler = () => popup.classList.remove('popup_opened')
+const formCloseHandler = () => popup.classList.remove('popup_opened')
 
 formElement.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', formOpenHandler);
 closeButton.addEventListener('click', formCloseHandler);
+
+// create initial cards
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.elements');
+
+initialCards.forEach(item => {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = item.link;
+  card.querySelector('.card__title').textContent = item.name;
+  cardsContainer.append(card);
+})
