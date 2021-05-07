a5496a073fd0fbbfedd9f6f67c5cfba5580545b1
Sergey Fomenko <fomenko.personal@gmail.com>
2021-05-07 13:17:51 +0000

    feat: add image popup

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,9 @@ const description = document.querySelector('.profile__description');
 const nameInput = editForm.querySelector('#name');
 const descriptionInput = editForm.querySelector('#description');
 
+const imagePopup = document.querySelector('.popup_type_image');
+const imageCloseButton = imagePopup.querySelector('.popup__button');
+
 const placeInput = addForm.querySelector('#place');
 const linkInput = addForm.querySelector('#link');
 
@@ -64,8 +67,10 @@ const removeCard = button => {
 const addListeners = card => {
   const likeButton = card.querySelector('.card__button_type_like');
   const removeButton = card.querySelector('.card__button_type_remove');
+  const image = card.querySelector('.card__image');
   getLike(likeButton);
   removeCard(removeButton);
+  getImage(image);
 }
 
 // edit user info
@@ -92,6 +97,7 @@ editCloseButton.addEventListener('click', editFormCloseHandler);
 getCard = () => {
   const card = cardTemplate.querySelector('.card').cloneNode(true);
   card.querySelector('.card__image').src = linkInput.value;
+  card.querySelector('.card__image').alt = placeInput.value;
   card.querySelector('.card__title').textContent = placeInput.value;
   addListeners(card);
   return card;
@@ -115,10 +121,30 @@ addButton.addEventListener('click', addFormOpenHandler);
 addForm.addEventListener('submit', addFormSubmitHandler);
 addCloseButton.addEventListener('click', addFormCloseHandler);
 
+// image popup
+const getImage = image => {
+  image.addEventListener('click', (evt) => {
+    const card = evt.target.closest('.card');
+    const cardImage = card.querySelector('.card__image');
+    const cardDescription = card.querySelector('.card__title');
+    const popupImage = imagePopup.querySelector('.image__item');
+    const popupDescription = imagePopup.querySelector('.image__description');
+    popupImage.src = cardImage.src;
+    popupImage.alt = cardDescription.textContent;
+    popupDescription.textContent = cardDescription.textContent;
+    togglePopup(imagePopup);
+  });
+};
+
+const imageFormCloseHandler = () => imagePopup.classList.remove('popup_opened')
+
+imageCloseButton.addEventListener('click', imageFormCloseHandler);
+
 // create initial cards
 initialCards.forEach(item => {
   const card = cardTemplate.querySelector('.card').cloneNode(true);
   card.querySelector('.card__image').src = item.link;
+  card.querySelector('.card__image').alt = item.name;
   card.querySelector('.card__title').textContent = item.name;
   addListeners(card);
   cardsContainer.append(card);
